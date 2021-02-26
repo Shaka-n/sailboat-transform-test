c05d05f18e074246636d7010829d55534f45a182
Emma Hyde <emma.hyde@endurance.com>
2021-02-26 18:21:06 -0500

    easier testing

--- calculation.rb
+++ calculation.rb
@@ -3,11 +3,10 @@ class Calculation
 
   def initialize
     @variance = 0
-    @eta = 0.01
+    @eta = 1
   end
 
-  def determine_apparent_wind_angle(true_wind, wind_velocity)
-    true_wind_radians = true_wind.to_radians
+  def determine_apparent_wind_radians(true_wind_radians, wind_velocity)
     equation_1 = wind_velocity * @eta
     maybe_apparent_wind_radians = true_wind_radians - 0.0001
 
@@ -21,10 +20,10 @@ class Calculation
     maybe_apparent_wind_radians
   end
 
-  def sailboat_transform(true_wind_angle, apparent_wind_angle)
-    Math.sin(true_wind_angle) * Math.sin(apparent_wind_angle) * (
-      Math.sin(apparent_wind_angle / 2) /
-        Math.sin(true_wind_angle - apparent_wind_angle)
+  def sailboat_transform(true_wind_radians, apparent_wind_radians)
+    Math.sin(true_wind_radians) * Math.sin(apparent_wind_radians) * (
+      Math.sin(apparent_wind_radians / 2) /
+        Math.sin(true_wind_radians - apparent_wind_radians)
     ) ** 2
   end
 end
@@ -41,18 +40,24 @@ class Numeric
   end
 end
 
+real_wind_degrees = 45
+wind_velocity = 10
 
 calculator = Calculation.new
 
 puts "real wind angle"
-puts 1
+puts real_wind_degrees
 
 puts "wind velocity"
-puts 10
+puts wind_velocity
 
 puts "apparent wind angle"
-apparent_wind_radians = calculator.determine_apparent_wind_angle(1, 10)
+apparent_wind_radians = calculator.determine_apparent_wind_radians(
+  real_wind_degrees.to_radians,
+  wind_velocity
+)
 puts apparent_wind_radians.to_degrees
 
 puts "boat speed"
-puts 10 * ((Math.sin(1.to_radians - apparent_wind_radians) / apparent_wind_radians).to_degrees)
+puts(wind_velocity * Math.sin(real_wind_degrees.to_radians - apparent_wind_radians).to_degrees /
+    apparent_wind_radians.to_degrees)
